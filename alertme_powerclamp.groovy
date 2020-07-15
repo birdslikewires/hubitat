6efba8f6cef6b6b6cc5ed37f49d30440ad0f824b
Andrew Davison <andydvsn@users.noreply.github.com>
2020-07-15 14:20:33 +0100

    Update alertme_powerclamp.groovy
    The Power Clamp is feature complete now, I think. There's a little more data, such as a counter, to extract from the status data, but it's not terribly useful.

--- alertme_powerclamp.groovy
+++ alertme_powerclamp.groovy
@@ -15,6 +15,13 @@ metadata {
 		//capability "Refresh"
         capability "Temperature Measurement"
 
+        attribute "batteryWithUnit", "string"
+        attribute "batteryVoltage", "string"
+        attribute "batteryVoltageWithUnit", "string"
+        attribute "deviceType", "string"
+        attribute "powerWithUnit", "string"
+        attribute "temperatureWithUnit", "string"
+
 		fingerprint profileId: "C216", inClusters: "00F0,00F3,00EF", outClusters: "", manufacturer: "AlertMe.com", model: "Power Clamp", deviceJoinName: "AlertMe Power Clamp"
         
 	}
@@ -24,79 +31,89 @@ metadata {
 
 preferences {
 	
-	input name: "debugEnable", type: "bool", title: "Enable debug logging", defaultValue: true
-	input name: "logEnable", type: "bool", title: "Enable logging", defaultValue: true
+	//input name: "powerOffset", type: "int", title: "Power Offset", description: "Offset in Watts (default: 0)", defaultValue: "0"
+	input name: "vMinSetting",type: "decimal",title: "Battery Minimum Voltage",description: "Low battery voltage (default: 2.5)",defaultValue: "2.5",range: "2.1..2.8"
+    input name: "vMaxSetting",type: "decimal",title: "Battery Maximum Voltage",description: "Full battery voltage (default: 3.0)",defaultValue: "3.0",range: "2.9..3.4"
+	input name: "infoLogging",type: "bool",title: "Enable logging",defaultValue: true
+	input name: "debugLogging",type: "bool",title: "Enable debug logging",defaultValue: true
 	
 }
-
-
 
 def initialize() {
 	
 	configure()
-	log.warn "Initialize Called!"
+	logging("Initialise Called!",100)
 	
 }
 
-def logsOff(){
+def debugLogOff(){
 
-	device.updateSetting("debugEnable",[value:"false",type:"bool"])
-	log.warn "Debug logging disabled!"
+	device.updateSetting("debugLogging",[value:"false",type:"bool"])
+	logging("Debug logging disabled.",101)
 
 }
 
 def updated(){
 
-	log.info "Update called!"
+	unschedule()
 
-	log.warn "debug logging is: ${debugEnable == true}"
-	log.warn "description logging is: ${logEnable == true}"
+	logging("Info logging is: ${infoLogging == true}",101)
+	logging("Debug logging is: ${debugLogging == true}",101)
 	
-	if (debugEnable) runIn(1800,logsOff)
+	if (debugLogging) runIn(1800,debugLogOff)
 
 }
 
 def parse(String description) {
-	
+	    
     def descriptionMap = zigbee.parseDescriptionAsMap(description)
     
 	if (descriptionMap) {
 	
-        if (debugEnable) log.debug "splurge: ${descriptionMap}"        
+        logging("splurge: ${descriptionMap}",1)
 		outputValues(descriptionMap)
 
 	} else {
 		
-		log.warn "PARSE FAILED: $description"
+		logging("PARSE FAILED: $description",101)
 
 	}	
 
 }
 
 def refresh() {
-	
-	if (debugEnable) log.debug "There's really no refresh trigger for this device."
+
+    def fixedDescription = "catchall: C216 00F0 02 02 0040 00 B893 01 00 0000 FB 01 1BBC5F621C870B1401B4FF0000"
+    logging("Attempting to parse fixed data where battery = 2.951 and temperature = 17.25",1)
+    parse(fixedDescription)
 
 }
 
 def configure() {
 
-	if (debugEnable) log.debug "There's really nothing to be configured on this device."
+	logging("There's really nothing to be configured on this device.",1)
 
 }
 
 def randomRefreshAttempt() {
 
-    def thing = "dunno"
-    //thing = zigbee.readAttribute(0x00F0, 0x0000)
+    def fixedDescription = "C216 00F0 02 02 0040 00 B893 01 00 0000 FB 01 1BB31D571C870B1401B4FF0000"
     
-	if (debugEnable) log.debug thing
+    parse(fixedDescription)
+    
+	logging("Attempting to parse fixed data...",1)
 
 }
 
 def outputValues(map) {
 
 	String[] receivedData = map.data
+
+	Map alertMeDevices = [
+	    '1B': 'Power Clamp',
+	    '1C': 'Switch',
+	    '1D': 'Key Fob'
+	]
 
 	if (map.clusterId == "00EF") {
 
@@ -106,46 +123,141 @@ def outputValues(map) {
 
 			// If we receive two 8-bit values, we know it's the instant reading. 
 
-			def powerValue = "undefined"
-			powerValue = receivedData[0..1].reverse().join()
-			powerValue = zigbee.convertHexToInt(powerValue)
-			if (logEnable) log.info "$device reports $powerValue W"
+			def powerValueHex = "undefined"
+			def int powerValue = 0
 
-			return [name: "power", value: powerValue]
+			powerValueHex = receivedData[0..1].reverse().join()
+			logging("${device} : power byte flipped : ${powerValueHex}",1)
+
+			powerValue = zigbee.convertHexToInt(powerValueHex)
+			logging("${device} : power sensor reports : ${powerValue}",1)
+
+			//if (powerOffset != null) powerValue = powerValue + powerOffset
+			logging("${device} : Power Usage : ${powerValue} W",100)
+
+			sendEvent(name: "power", value: powerValue, unit: "W", isStateChange: true)
+			sendEvent(name: "powerWithUnit", value: "${powerValue} W", isStateChange: true)
 
 		} else if (receivedData.length == 9) {
 
 			// Nine 8-bit values and we've received the power usage summary. Not seen anyone work this one out yet.
 
-			log.info "Power usage summary data received, but we don't know how to decipher this yet."
-			log.info "Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}"
+			logging("Power usage summary data received, but we don't know how to decipher this yet.",1)
+			logging("Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}",1)
 
 		} else {
 
-			log.warn "Unknown power usage information! Please report this to the developer."
-			log.warn "Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}"
+			logging("Unknown power usage information! Please report this to the developer.",101)
+			logging("Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}",101)
 
 		}
 
 	} else if (map.clusterId == "00F0") {
 
-		// Cluster 00F0 deals with battery and temperature data.
+		// Cluster 00F0 deals with device status, including battery and temperature data.
 
-		log.info "Battery and temperature data received, but we don't know how to decipher this yet."
-		log.info "Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}"
+		// Look up the real device type from the AlertMe device map.
+        def deviceValue = receivedData[0]
+        sendEvent(name:"deviceType",value:alertMeDevices[deviceValue],isStateChange: false)
+
+        // Report the battery voltage and calculated percentage.
+		def batteryValue = "undefined"
+		batteryValue = receivedData[5..6].reverse().join()
+		logging("${device} : batteryValue byte flipped : ${batteryValue}",1)
+		batteryValue = zigbee.convertHexToInt(batteryValue) / 1000
+		parseAndSendBatteryStatus(batteryValue)
+		sendEvent(name:"batteryVoltage", value: batteryValue, unit: "V", isStateChange: false)
+		sendEvent(name:"batteryVoltageWithUnit", value: "${batteryValue} V", isStateChange: true)
+
+		def temperatureValue = "undefined"
+		temperatureValue = receivedData[7..8].reverse().join()
+		logging("${device} : temperatureValue byte flipped : ${temperatureValue}",1)
+        temperatureValue = zigbee.convertHexToInt(temperatureValue) / 16
+		logging("${device} : Temperature : ${temperatureValue} C",100)
+
+		sendEvent(name:"temperature", value: temperatureValue, unit: "C", isStateChange: false)
+		sendEvent(name:"temperatureWithUnit", value: "${temperatureValue} °C", unit: "C", isStateChange: false)
 
 	} else if (map.clusterId == "00F3") {
 
-		log.info "I think this is the tamper button status. We don't know what to do with this yet."
-		log.info "Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}"
+		logging("I think this is the tamper button status. We don't know what to do with this yet.",100)
+		logging("Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}",100)
 
 	} else {
 
-		log.warn "Unknown cluster received! Please report this to the developer."
-		log.warn "Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}"
+		logging("Unknown cluster received! Please report this to the developer.",101)
+		logging("Received clusterId ${map.clusterId} with ${receivedData.length} values: ${receivedData}",101)
 
 	}
 
 	return null
+
+}
+
+void parseAndSendBatteryStatus(BigDecimal vCurrent) {
+
+    BigDecimal bat = 0
+    BigDecimal vMin = vMinSetting == null ? 2.5 : vMinSetting
+    BigDecimal vMax = vMaxSetting == null ? 3.0 : vMaxSetting    
+
+    if(vMax - vMin > 0) {
+        bat = ((vCurrent - vMin) / (vMax - vMin)) * 100.0
+    } else {
+        bat = 100
+    }
+    bat = bat.setScale(0, BigDecimal.ROUND_HALF_UP)
+    bat = bat > 100 ? 100 : bat
+    
+    vCurrent = vCurrent.setScale(3, BigDecimal.ROUND_HALF_UP)
+
+    logging("${device} : Battery : $bat% ($vCurrent V)", 100)
+    sendEvent(name:"battery",value:bat,unit: "%", isStateChange: false)
+    sendEvent(name:"batteryWithUnit",value:"${bat} %",isStateChange: false)
+
+}
+
+private boolean logging(message, level) {
+
+    boolean didLog = false
+     
+    Integer logLevelLocal = 0
+
+    if (infoLogging == null || infoLogging == true) {
+        logLevelLocal = 100
+    }
+
+    if (debugLogging == true) {
+        logLevelLocal = 1
+    }
+     
+    if (logLevelLocal != 0){
+
+        switch (logLevelLocal) {
+	        case 1:  
+	            if (level >= 1 && level < 99) {
+	                log.debug "$message"
+	                didLog = true
+	            } else if (level == 100) {
+	                log.info "$message"
+	                didLog = true
+	            } else if (level > 100) {
+					log.warn "$message"
+	                didLog = true
+	            }
+	        break
+	        case 100:  
+	            if (level == 100) {
+	                log.info "$message"
+	                didLog = true
+	            } else if (level > 100) {
+					log.warn "$message"
+	                didLog = true
+	            }
+	        break
+        }
+
+    }
+
+    return didLog
 
 }
