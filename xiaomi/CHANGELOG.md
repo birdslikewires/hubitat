d36ba44215effaca102b533fe13199616d926018
Andrew Davison <andydvsn@users.noreply.github.com>
2021-12-21 02:43:17 +0000

    First roll at a WXKG11LM driver

---
+++ xiaomi/CHANGELOG.md
@@ -0,0 +1,51 @@
+# IKEA Symfonisk and Trådfri Drivers for Hubitat - Changelog
+
+## 20th December 2021
+
+- Calculate the device presence timeout threshold automatically.
+- Tidied init process.
+
+## 24th November 2021
+
+- Corrected silly error with the battery voltage report, though I don't know if it will ultimately help with the readings.
+- Reporting intervals and presence detection intervals are now specified in minutes.
+
+## 24th October 2021
+
+- Uncoupled battery state from presence detection.
+- Skip device announcement broadcast messages.
+
+## 18th October 2021
+
+- Increase reporting interval from every hour to every two hours.
+
+## 10th August 2021
+
+- Mimic the physical button push when triggered on/off in software, otherwise automations won't run.
+
+## 5th August 2021
+
+- Added switch on/off for compatibility with Mirror app (E1744).
+- Remote can now be used as a lighting dimmer controller (E1744).
+
+## 4th August 2021
+
+- Added the Symfonisk Remote (E1744) driver.
+- Presents as a three button device for press, clockwise and anticlockwise rotation.
+- Pushed, held and released are recognised for button presses and rotations.
+- Official method for double tap supported, triple tap shown as a release event.
+- Battery data from the device seems to be nonsense, so we're fudging it.
+
+## 30th July 2021
+
+- General cleanup and nonsense removal.
+- Simulated push / hold / release events are now supported.
+- Remarkably, everything held up overnight and appears to work!
+
+## 29th July 2021
+
+- Had a whirl at making my own Shortcut Button (E1812) driver.
+- Press, hold and release are all recognised and debounced.
+- Battery reporting configured for a message every hour.
+- Presence detection relies upon the battery report; as these are sleepy endpoints with small batteries I'm trialling hourly reports and we'll see how long they last. As such presence sort-of-works, but needs some more tweaking.
+- Plenty of tidying to be done, as this was based upon my AlertMe Button driver which has a bit more to cope with.
---
+++ xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg11lm.groovy
@@ -0,0 +1,459 @@
+/*
+ * 
+ *  Xiaomi Aqara Wireless Mini Switch WXKG11LM Driver v1.00 (20th December 2021)
+ *	
+ */
+
+
+import groovy.transform.Field
+
+@Field boolean debugMode = true
+
+@Field int reportIntervalMinutes = 50		// How often the device should be reporting in.
+
+metadata {
+
+	definition (name: "Xiaomi Aqara Wireless Mini Switch WXKG11LM", namespace: "BirdsLikeWires", author: "Andrew Davison", importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/master/xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg11lm.groovy") {
+
+		capability "Battery"
+		capability "Configuration"
+		capability "DoubleTapableButton"
+		capability "Initialize"
+		capability "PresenceSensor"
+		capability "PushableButton"
+		capability "Refresh"
+
+		attribute "batteryState", "string"
+		attribute "batteryVoltage", "string"
+		attribute "batteryVoltageWithUnit", "string"
+		attribute "batteryWithUnit", "string"
+
+		if (debugMode) {
+			command "checkPresence"
+		}
+
+		fingerprint profileId: "0104", inClusters: "0000,FFFF,0006", outClusters: "0000,0004,FFFF", manufacturer: "LUMI", model: "lumi.sensor_switch.aq2", deviceJoinName: "Aqara Wireless Mini Switch", application: "03"
+
+	}
+
+}
+
+
+preferences {
+	
+	input name: "infoLogging", type: "bool", title: "Enable logging", defaultValue: true
+	input name: "debugLogging", type: "bool", title: "Enable debug logging", defaultValue: true
+	input name: "traceLogging", type: "bool", title: "Enable trace logging", defaultValue: true
+	
+}
+
+
+def installed() {
+	// Runs after first installation.
+	logging("${device} : Installed", "info")
+	configure()
+	initialize()
+}
+
+
+def configure() {
+
+	unschedule()
+
+	// Default logging preferences.
+	device.updateSetting("infoLogging",[value:"true",type:"bool"])
+	device.updateSetting("debugLogging",[value:"true",type:"bool"])
+	device.updateSetting("traceLogging",[value:"true",type:"bool"])
+
+	// Schedule the presence check.
+	int checkEveryMinutes = 10																// Check presence timestamp every 10 minutes.						
+	randomSixty = Math.abs(new Random().nextInt() % 60)
+	schedule("${randomSixty} 0/${checkEveryMinutes} * * * ? *", checkPresence)				// At X seconds past the minute, every checkEveryMinutes minutes.
+
+	// Configuration complete.
+	logging("${device} : Configured", "info")
+
+}
+
+
+def initialize() {
+
+	state.clear()
+	state.presenceUpdated = 0
+
+	sendEvent(name: "presence", value: "present", isStateChange: false)
+
+	updated()
+
+	// Initialisation complete.
+	logging("${device} : Initialised", "info")
+
+}
+
+
+def updated() {
+	// Runs whenever preferences are saved.
+
+	if (!debugMode) {
+		runIn(3600,infoLogOff)
+		runIn(2400,debugLogOff)
+		runIn(1200,traceLogOff)
+	}
+
+	loggingStatus()
+	refresh()
+
+}
+
+
+void loggingStatus() {
+
+	log.info "${device} : Logging : ${infoLogging == true}"
+	log.debug "${device} : Debug Logging : ${debugLogging == true}"
+	log.trace "${device} : Trace Logging : ${traceLogging == true}"
+
+}
+
+
+void traceLogOff(){
+	
+	log.trace "${device} : Trace Logging : Automatically Disabled"
+	device.updateSetting("traceLogging",[value:"false",type:"bool"])
+
+}
+
+void debugLogOff(){
+	
+	log.debug "${device} : Debug Logging : Automatically Disabled"
+	device.updateSetting("debugLogging",[value:"false",type:"bool"])
+
+}
+
+
+void infoLogOff(){
+	
+	log.info "${device} : Info Logging : Automatically Disabled"
+	device.updateSetting("infoLogging",[value:"false",type:"bool"])
+
+}
+
+
+void reportToDev(map) {
+
+	String[] receivedData = map.data
+
+	def receivedDataCount = ""
+	if (receivedData != null) {
+		receivedDataCount = "${receivedData.length} bits of "
+	}
+
+	logging("${device} : UNKNOWN DATA! Please report these messages to the developer.", "warn")
+	logging("${device} : Received : cluster: ${map.cluster}, clusterId: ${map.clusterId}, attrId: ${map.attrId}, command: ${map.command} with value: ${map.value} and ${receivedDataCount}data: ${receivedData}", "warn")
+	logging("${device} : Splurge! : ${map}", "trace")
+
+}
+
+
+void push(buttonId) {
+	
+	sendEvent(name:"pushed", value: buttonId, isStateChange:true)
+	
+}
+
+
+void doubleTap(buttonId) {
+	
+	sendEvent(name:"doubleTapped", value: buttonId, isStateChange:true)
+	
+}
+
+
+void refresh() {
+
+	logging("${device} : Refreshing", "info")
+
+}
+
+
+def updatePresence() {
+
+	long millisNow = new Date().time
+	state.presenceUpdated = millisNow
+
+}
+
+
+def checkPresence() {
+
+	// Check how long ago the presence state was updated.
+
+	int uptimeAllowanceMinutes = 20			// The hub takes a while to settle after a reboot.
+
+	if (state.presenceUpdated > 0) {
+
+		long millisNow = new Date().time
+		long millisElapsed = millisNow - state.presenceUpdated
+		long presenceTimeoutMillis = ((reportIntervalMinutes * 2) + 20) * 60000
+		long reportIntervalMillis = reportIntervalMinutes * 60000
+		BigInteger secondsElapsed = BigDecimal.valueOf(millisElapsed / 1000)
+		BigInteger hubUptime = location.hub.uptime
+
+		if (millisElapsed > presenceTimeoutMillis) {
+
+			if (hubUptime > uptimeAllowanceMinutes * 60) {
+
+				sendEvent(name: "presence", value: "not present")
+				logging("${device} : Presence : Not Present! Last report received ${secondsElapsed} seconds ago.", "warn")
+
+			} else {
+
+				logging("${device} : Presence : Ignoring overdue presence reports for ${uptimeAllowanceMinutes} minutes. The hub was rebooted ${hubUptime} seconds ago.", "debug")
+
+			}
+
+		} else {
+
+			sendEvent(name: "presence", value: "present")
+			logging("${device} : Presence : Last presence report ${secondsElapsed} seconds ago.", "debug")
+
+		}
+
+		logging("${device} : checkPresence() : ${millisNow} - ${state.presenceUpdated} = ${millisElapsed}", "trace")
+		logging("${device} : checkPresence() : Report interval is ${reportIntervalMillis} ms, timeout is ${presenceTimeoutMillis} ms.", "trace")
+
+	} else {
+
+		logging("${device} : Presence : Waiting for first presence report.", "warn")
+
+	}
+
+}
+
+
+def parse(String description) {
+
+	// Primary parse routine.
+
+	logging("${device} : Parse : $description", "debug")
+
+	updatePresence()
+
+	Map descriptionMap = null
+
+	if (description.indexOf('encoding: 10') >= 0 || description.indexOf('encoding: 20') >= 0) {
+
+		// Normal encoding should bear some resemblance to the Zigbee Cluster Library Specification
+		descriptionMap = zigbee.parseDescriptionAsMap(description)
+
+	} else {
+
+		// Anything else is specific to Xiaomi, so we'll just slice and dice the string we receive.
+		descriptionMap = description.split(', ').collectEntries {
+			entry -> def pair = entry.split(': ')
+			[(pair.first()): pair.last()]
+		}
+
+	}
+
+	if (descriptionMap) {
+
+		processMap(descriptionMap)
+
+	} else {
+		
+		logging("${device} : Parse : Failed to parse received data. Please report these messages to the developer.", "warn")
+		logging("${device} : Splurge! : ${description}", "warn")
+
+	}
+
+}
+
+
+def processMap(Map map) {
+
+	logging("${device} : processMap() : ${map}", "trace")
+
+	String[] receivedData = map.data
+
+	if (map.cluster == "0000") { 
+
+		if (map.attrId == "0005" || map.attrId == "FF01") {
+
+			def batteryData = ""
+
+			if (map.attrId == "0005") {
+
+				// Grab battery data triggered by short press of the reset button.
+				batteryData = map.value.split('FF42')[1]
+
+				// Scrounge more value! It's another button, so as these can quad-click we'll call this button 5.
+				logging("${device} : Trigger : Button 5 Pressed", "info")
+				sendEvent(name: "pushed", value: 5, isStateChange: true)
+
+			} else {
+
+				// We get data with an attrId of FF01 when pressed more than twice, but only the check-in reports contain battery info.
+				def dataSize = map.value.size()
+
+				if (dataSize > 20) {
+					batteryData = map.value
+				} else {
+					logging("${device} : batteryData : No battery information in this report.", "debug")
+					return
+				}
+				
+			}
+
+			// Report the battery voltage and calculated percentage.
+			def batteryVoltageHex = "undefined"
+			BigDecimal batteryVoltage = 0
+
+			batteryVoltageHex = batteryData[8..9] + batteryData[6..7]
+			logging("${device} : batteryVoltageHex : ${batteryVoltageHex}", "trace")
+
+			batteryVoltage = zigbee.convertHexToInt(batteryVoltageHex)
+			logging("${device} : batteryVoltage sensor value : ${batteryVoltage}", "debug")
+
+			batteryVoltage = batteryVoltage.setScale(2, BigDecimal.ROUND_HALF_UP) / 1000
+
+			logging("${device} : batteryVoltage : ${batteryVoltage}", "debug")
+			sendEvent(name: "batteryVoltage", value: batteryVoltage, unit: "V")
+			sendEvent(name: "batteryVoltageWithUnit", value: "${batteryVoltage} V")
+
+			BigDecimal batteryPercentage = 0
+			BigDecimal batteryVoltageScaleMin = 2.1
+			BigDecimal batteryVoltageScaleMax = 3.0
+
+			if (batteryVoltage >= batteryVoltageScaleMin) {
+
+				state.batteryOkay = true
+
+				batteryPercentage = ((batteryVoltage - batteryVoltageScaleMin) / (batteryVoltageScaleMax - batteryVoltageScaleMin)) * 100.0
+				batteryPercentage = batteryPercentage.setScale(0, BigDecimal.ROUND_HALF_UP)
+				batteryPercentage = batteryPercentage > 100 ? 100 : batteryPercentage
+
+				if (batteryPercentage > 20) {
+					logging("${device} : Battery : $batteryPercentage% ($batteryVoltage V)", "info")
+				} else {
+					logging("${device} : Battery : $batteryPercentage% ($batteryVoltage V)", "warn")
+				}
+
+				sendEvent(name: "battery", value:batteryPercentage, unit: "%")
+				sendEvent(name: "batteryWithUnit", value:"${batteryPercentage} %")
+				sendEvent(name: "batteryState", value: "discharging")
+
+			} else {
+
+				// Very low voltages indicate an exhausted battery which requires replacement.
+
+				state.batteryOkay = false
+
+				batteryPercentage = 0
+
+				logging("${device} : Battery : Exhausted battery requires replacement.", "warn")
+				logging("${device} : Battery : $batteryPercentage% ($batteryVoltage V)", "warn")
+				sendEvent(name: "battery", value:batteryPercentage, unit: "%")
+				sendEvent(name: "batteryWithUnit", value:"${batteryPercentage} %")
+				sendEvent(name: "batteryState", value: "exhausted")
+
+			}
+
+		} else {
+
+			// Not a clue what we've received.
+			reportToDev(map)
+
+		}
+
+	} else if (map.cluster == "0006") { 
+
+		int buttonNumber = map.value[-1..-1].toInteger()
+		buttonNumber = buttonNumber == 0 ? 1 : buttonNumber
+
+		if (buttonNumber == 2) {
+			logging("${device} : Trigger : Button 1 Double Tapped", "info")
+			sendEvent(name: "doubleTapped", value: 1, isStateChange: true)
+		}
+
+		logging("${device} : Trigger : Button ${buttonNumber} Pressed", "info")
+		sendEvent(name: "pushed", value: buttonNumber, isStateChange: true)
+
+	} else {
+
+		// Not a clue what we've received.
+		reportToDev(map)
+
+	}
+
+	return null
+
+}
+
+
+void sendZigbeeCommands(List<String> cmds) {
+
+	// All hub commands go through here for immediate transmission and to avoid some method() weirdness.
+
+    logging("${device} : sendZigbeeCommands received : ${cmds}", "trace")
+    sendHubCommand(new hubitat.device.HubMultiAction(cmds, hubitat.device.Protocol.ZIGBEE))
+
+}
+
+
+private String[] millisToDhms(int millisToParse) {
+
+	long secondsToParse = millisToParse / 1000
+
+	def dhms = []
+	dhms.add(secondsToParse % 60)
+	secondsToParse = secondsToParse / 60
+	dhms.add(secondsToParse % 60)
+	secondsToParse = secondsToParse / 60
+	dhms.add(secondsToParse % 24)
+	secondsToParse = secondsToParse / 24
+	dhms.add(secondsToParse % 365)
+	return dhms
+
+}
+
+
+private BigDecimal hexToBigDecimal(String hex) {
+
+    int d = Integer.parseInt(hex, 16) << 21 >> 21
+    return BigDecimal.valueOf(d)
+
+}
+
+
+private boolean logging(String message, String level) {
+
+	boolean didLog = false
+
+	if (level == "error") {
+		log.error "$message"
+		didLog = true
+	}
+
+	if (level == "warn") {
+		log.warn "$message"
+		didLog = true
+	}
+
+	if (traceLogging && level == "trace") {
+		log.trace "$message"
+		didLog = true
+	}
+
+	if (debugLogging && level == "debug") {
+		log.debug "$message"
+		didLog = true
+	}
+
+	if (infoLogging && level == "info") {
+		log.info "$message"
+		didLog = true
+	}
+
+	return didLog
+
+}
