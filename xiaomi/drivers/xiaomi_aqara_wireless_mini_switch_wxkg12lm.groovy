64f1bffbd674380d477999eac3ccdd5e959b9da7
Andrew Davison <andydvsn@users.noreply.github.com>
2022-06-26 23:13:30 +0100

    Update xiaomi_aqara_wireless_mini_switch_wxkg12lm.groovy
    - Add support for the WXKG11LM variants, as retaining a separate driver for those no longer makes sense given the functional similarities.
    - Ditch "withUnit" values. These increase hub load with unnecessary events.

--- xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg12lm.groovy
+++ xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg12lm.groovy
@@ -1,10 +1,11 @@
 /*
  * 
- *  Xiaomi Aqara Wireless Mini Switch WXKG12LM Driver v1.03 (11th January 2022)
+ *  Xiaomi Aqara Wireless Mini Switch WXKG11LM / WXKG12LM Driver v1.04 (26th June 2022)
  *	
  */
 
 
+#include BirdsLikeWires.library
 import groovy.transform.Field
 
 @Field boolean debugMode = false
@@ -13,7 +14,7 @@ import groovy.transform.Field
 
 metadata {
 
-	definition (name: "Xiaomi Aqara Wireless Mini Switch WXKG12LM", namespace: "BirdsLikeWires", author: "Andrew Davison", importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/master/xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg12lm.groovy") {
+	definition (name: "Xiaomi Aqara Wireless Mini Switch WXKG11LM / WXKG12LM", namespace: "BirdsLikeWires", author: "Andrew Davison", importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/master/xiaomi/drivers/xiaomi_aqara_wireless_mini_switch_wxkg12lm.groovy") {
 
 		capability "AccelerationSensor"
 		capability "Battery"
@@ -28,16 +29,15 @@ metadata {
 
 		attribute "batteryState", "string"
 		attribute "batteryVoltage", "string"
-		attribute "batteryVoltageWithUnit", "string"
-		attribute "batteryWithUnit", "string"
-		//attribute "temperatureWithUnit", "string"
 
 		if (debugMode) {
 			command "checkPresence"
 			command "testCommand"
 		}
 
-		fingerprint profileId: "0104", inClusters: "0000,0012,0006,0001", outClusters: "0000", manufacturer: "LUMI", model: "lumi.sensor_swit", deviceJoinName: "WXKG12LM", application: "05"
+		fingerprint profileId: "0104", inClusters: "0000,FFFF,0006", outClusters: "0000,0004,FFFF", manufacturer: "LUMI", model: "lumi.sensor_switch.aq2", deviceJoinName: "WXKG11LMr1"
+		fingerprint profileId: "0104", inClusters: "0000,0012,0003", outClusters: "0000", manufacturer: "LUMI", model: "lumi.remote.b1acn01", deviceJoinName: "WXKG11LMr2"
+		fingerprint profileId: "0104", inClusters: "0000,0012,0006,0001", outClusters: "0000", manufacturer: "LUMI", model: "lumi.sensor_switch.aq3", deviceJoinName: "WXKG12LM"
 
 	}
 
@@ -225,9 +225,22 @@ void processMap(Map map) {
 
 	String[] receivedData = map.data
 
-	if (map.cluster == "0012") { 
+	if (map.cluster == "0006") { 
+		// Here we handle button presses for the WXKG11LM 
 
-		// Here we handle button presses and holds.
+		int buttonNumber = map.value[-1..-1].toInteger()
+		buttonNumber = buttonNumber == 0 ? 1 : buttonNumber
+
+		if (buttonNumber == 2) {
+			logging("${device} : Trigger : Button Double Tapped", "info")
+			sendEvent(name: "doubleTapped", value: 1, isStateChange: true)
+		}
+
+		logging("${device} : Trigger : Button ${buttonNumber} Pressed", "info")
+		sendEvent(name: "pushed", value: buttonNumber, isStateChange: true)
+
+	} else if (map.cluster == "0012") { 
+		// Here we handle button presses for the WXKG12LM and holds for WXKG11LMr2
 
 		if (map.value == "0100") {
 
@@ -240,14 +253,14 @@ void processMap(Map map) {
 			sendEvent(name: "doubleTapped", value: 1, isStateChange: true)
 			sendEvent(name: "pushed", value: 2, isStateChange: true)
 
-		} else if (map.value == "1000") {
+		} else if (map.value == "0000" || map.value == "1000") {
 
 			state.changeLevelStart = now()
 			logging("${device} : Trigger : Button Held", "info")
 			sendEvent(name: "held", value: 1, isStateChange: true)
 			sendEvent(name: "pushed", value: 3, isStateChange: true)
 
-		} else if (map.value == "1100") {
+		} else if (map.value == "1100" || map.value == "FF00") {
 
 			logging("${device} : Trigger : Button Released", "info")
 			sendEvent(name: "released", value: 1, isStateChange: true)
@@ -327,7 +340,6 @@ void processMap(Map map) {
 
 			logging("${device} : batteryVoltage : ${batteryVoltage}", "debug")
 			sendEvent(name: "batteryVoltage", value: batteryVoltage, unit: "V")
-			sendEvent(name: "batteryVoltageWithUnit", value: "${batteryVoltage} V")
 
 			BigDecimal batteryPercentage = 0
 			BigDecimal batteryVoltageScaleMin = 2.1
@@ -348,7 +360,6 @@ void processMap(Map map) {
 				}
 
 				sendEvent(name: "battery", value:batteryPercentage, unit: "%")
-				sendEvent(name: "batteryWithUnit", value:"${batteryPercentage} %")
 				sendEvent(name: "batteryState", value: "discharging")
 
 			} else {
@@ -362,7 +373,6 @@ void processMap(Map map) {
 				logging("${device} : Battery : Exhausted battery requires replacement.", "warn")
 				logging("${device} : Battery : $batteryPercentage% ($batteryVoltage V)", "warn")
 				sendEvent(name: "battery", value:batteryPercentage, unit: "%")
-				sendEvent(name: "batteryWithUnit", value:"${batteryPercentage} %")
 				sendEvent(name: "batteryState", value: "exhausted")
 
 			}
@@ -376,7 +386,6 @@ void processMap(Map map) {
 			// logging("${device} : temperatureCelsius sensor value : ${temperatureCelsius}", "trace")
 			// logging("${device} : Temperature : $temperatureCelsius °C", "info")
 			// sendEvent(name: "temperature", value: temperatureCelsius, unit: "C")
-			// sendEvent(name: "temperatureWithUnit", value: "${temperatureCelsius} °C")
 
 		} else {
 
@@ -390,419 +399,5 @@ void processMap(Map map) {
 		reportToDev(map)
 
 	}
-
-}
-
-
-// Library v1.01 (11th January 2022)
-
-
-void sendZigbeeCommands(List<String> cmds) {
-	// All hub commands go through here for immediate transmission and to avoid some method() weirdness.
-
-    logging("${device} : sendZigbeeCommands received : ${cmds}", "trace")
-    sendHubCommand(new hubitat.device.HubMultiAction(cmds, hubitat.device.Protocol.ZIGBEE))
-
-}
-
-
-void updatePresence() {
-
-	long millisNow = new Date().time
-	state.presenceUpdated = millisNow
-	sendEvent(name: "presence", value: "present")
-
-}
-
-
-void checkPresence() {
-	// Check how long ago the presence state was updated.
-
-	int uptimeAllowanceMinutes = 20			// The hub takes a while to settle after a reboot.
-
-	if (state.presenceUpdated > 0) {
-
-		long millisNow = new Date().time
-		long millisElapsed = millisNow - state.presenceUpdated
-		long presenceTimeoutMillis = ((reportIntervalMinutes * 2) + 20) * 60000
-		long reportIntervalMillis = reportIntervalMinutes * 60000
-		BigInteger secondsElapsed = BigDecimal.valueOf(millisElapsed / 1000)
-		BigInteger hubUptime = location.hub.uptime
-
-		if (millisElapsed > presenceTimeoutMillis) {
-
-			if (hubUptime > uptimeAllowanceMinutes * 60) {
-
-				sendEvent(name: "presence", value: "not present")
-				logging("${device} : Presence : Not Present! Last report received ${secondsElapsed} seconds ago.", "warn")
-
-			} else {
-
-				logging("${device} : Presence : Ignoring overdue presence reports for ${uptimeAllowanceMinutes} minutes. The hub was rebooted ${hubUptime} seconds ago.", "debug")
-
-			}
-
-		} else {
-
-			sendEvent(name: "presence", value: "present")
-			logging("${device} : Presence : Last presence report ${secondsElapsed} seconds ago.", "debug")
-
-		}
-
-		logging("${device} : checkPresence() : ${millisNow} - ${state.presenceUpdated} = ${millisElapsed}", "trace")
-		logging("${device} : checkPresence() : Report interval is ${reportIntervalMillis} ms, timeout is ${presenceTimeoutMillis} ms.", "trace")
-
-	} else {
-
-		logging("${device} : Presence : Waiting for first presence report.", "warn")
-
-	}
-
-}
-
-
-void processBasic(Map map) {
-	// Process the basic descriptors normally received from Zigbee Cluster 0000 into device data values.
-
-	if (map.attrId == "0001") {
-
-		updateDataValue("application", "${map.value}")
-		logging("${device} : Application : ${map.value}", "debug")
-
-	} else if (map.attrId == "0004") {
-
-		updateDataValue("manufacturer", map.value)
-		logging("${device} : Manufacturer : ${map.value}", "debug")
-
-	} else if (map.attrId == "0005") {
-
-		updateDataValue("model", map.value)
-		logging("${device} : Model : ${map.value}", "debug")
-
-	} else if (map.attrId == "4000") {
-
-		updateDataValue("softwareBuild", "${map.value}")
-		logging("${device} : Firmware : ${map.value}", "debug")
-
-	}
-
-}
-
-
-void processDescriptors(Map map) {
-	// Process the simple descriptors normally received from Zigbee Cluster 8004 into device data values.
-
-	String[] receivedData = map.data
-
-	if (receivedData[1] == "00") {
-		// Received simple descriptor data.
-
-		//updateDataValue("endpointId", receivedData[5])						// can lead to a weird duplicate
-		updateDataValue("profileId", receivedData[6..7].reverse().join())
-
-		Integer inClusterNum = Integer.parseInt(receivedData[11], 16)
-		Integer position = 12
-		Integer positionCounter = null
-		String inClusters = ""
-		if (inClusterNum > 0) {
-			(1..inClusterNum).each() {b->
-				positionCounter = position+((b-1)*2)
-				inClusters += receivedData[positionCounter..positionCounter+1].reverse().join()
-				if (b < inClusterNum) {
-					inClusters += ","
-				}
-			}
-		}
-		position += inClusterNum*2
-		Integer outClusterNum = Integer.parseInt(receivedData[position], 16)
-		position += 1
-		String outClusters = ""
-		if (outClusterNum > 0) {
-			(1..outClusterNum).each() {b->
-				positionCounter = position+((b-1)*2)
-				outClusters += receivedData[positionCounter..positionCounter+1].reverse().join()
-				if (b < outClusterNum) {
-					outClusters += ","
-				}
-			}
-		}
-
-		updateDataValue("inClusters", inClusters)
-		updateDataValue("outClusters", outClusters)
-
-		logging("${device} : Received $inClusterNum inClusters : $inClusters", "debug")
-		logging("${device} : Received $outClusterNum outClusters : $outClusters", "debug")
-
-	} else {
-
-		reportToDev(map)
-
-	}
-
-}
-
-
-void reportToDev(map) {
-
-	String[] receivedData = map.data
-
-	def receivedDataCount = ""
-	if (receivedData != null) {
-		receivedDataCount = "${receivedData.length} bits of "
-	}
-
-	logging("${device} : UNKNOWN DATA! Please report these messages to the developer.", "warn")
-	logging("${device} : Received : endpoint: ${map.endpoint}, cluster: ${map.cluster}, clusterId: ${map.clusterId}, attrId: ${map.attrId}, command: ${map.command} with value: ${map.value} and ${receivedDataCount}data: ${receivedData}", "warn")
-	logging("${device} : Splurge! : ${map}", "trace")
-
-}
-
-
-@Field static Boolean debouncingParentState = false
-void debounceParentState(String attribute, String state, String message, String level, Integer duration) {
-
-	if (debouncingParentState) return
-	debouncingParentState = true
-
-	sendEvent(name: "$attribute", value: "$state")
-	logging("${device} : $message", "$level")
-
-	pauseExecution duration
-	debouncingParentState = false
-
-}
-
-
-def fetchChild(String type, String endpoint) {
-	// Creates and retrieves child devices matched to endpoints.
-
-	def childDevice = getChildDevice("${device.id}-${endpoint}")
-
-	if (endpoint != "null") {
-
-		if (!childDevice) {
-
-			logging("${device} : Creating child device $device.id-$endpoint", "debug")
-
-			childDevice = addChildDevice("hubitat", "Generic Component ${type}", "${device.id}-${endpoint}", [name: "${device.displayName} ${type} ${endpoint}", label: "${device.displayName} ${type} ${endpoint}", isComponent: false])
-
-			if (type == "Switch") {
-
-				// We could use this as an opportunity to set all the relays to a known state, but we don't. Just in case.
-				childDevice.parse([[name: "switch", value: 'off']])
-
-			} else {
-
-				logging("${device} : fetchChild() : I don't know what to do with the '$type' device type.", "error")
-
-			}
-
-			childDevice.updateSetting("txtEnable", false)
-
-		}
-
-		logging("${device} : Retrieved child device $device.id-$endpoint", "debug")
-
-	} else {
-
-		logging("${device} : Received null endpoint for device $device.id", "error")
-
-	}
-
-	return childDevice
-
-}
-
-
-def fetchChildStates(String state, String requestor) {
-	// Retrieves requested states of child devices.
-
-	logging("${device} : fetchChildStates() : Called by $requestor", "debug")
-
-	def childStates = []
-	def children = getChildDevices()
-
-	children.each {child->
-
-		// Give things a chance!
-		pauseExecution(100)
-	
-		// Grabs the requested state from the child device.
-		String childState = child.currentValue("${state}")
-
-		if ("${child.id}" != "${requestor}") {
-			// Don't include the requestor's state in the results, as we're likely in the process of updating it.
-			childStates.add("${childState}")
-			logging("${device} : fetchChildStates() : Found $child.id is '$childState'", "debug")
-		}
-
-	}
-
-	return childStates
-
-}
-
-
-void deleteChildren() {
-	// Deletes children we may have created.
-
-	logging("${device} : deleteChildren() : Deleting rogue children.", "debug")
-
-	def children = getChildDevices()
-    children.each {child->
-  		deleteChildDevice(child.deviceNetworkId)
-    }
-
-}
-
-
-void componentRefresh(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentRefresh() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he rattr 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x00 {}"])
-
-}
-
-
-void componentOn(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentOn() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he cmd 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x01 {}"])
-
-}
-
-
-void componentOff(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentOff() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he cmd 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x00 {}"])
-
-}
-
-
-private String flipLittleEndian(Map map, String attribute) {
-
-	String bigEndianAttribute = ""
-	for (int v = map."${attribute}".length(); v > 0; v -= 2) {
-		bigEndianAttribute += map."${attribute}".substring(v - 2, v)
-	}
-	return bigEndianAttribute
-
-}
-
-
-private String[] millisToDhms(int millisToParse) {
-
-	long secondsToParse = millisToParse / 1000
-
-	def dhms = []
-	dhms.add(secondsToParse % 60)
-	secondsToParse = secondsToParse / 60
-	dhms.add(secondsToParse % 60)
-	secondsToParse = secondsToParse / 60
-	dhms.add(secondsToParse % 24)
-	secondsToParse = secondsToParse / 24
-	dhms.add(secondsToParse % 365)
-	return dhms
-
-}
-
-
-private String percentageToHex(Integer pc) {
-
-	BigDecimal safePc = pc > 0 ? (pc*2.55) : 0
-	safePc = safePc > 255 ? 255 : safePc
-	return Integer.toHexString(safePc.intValue())
-
-}
-
-
-private Integer hexToPercentage(String hex) {
-
-	String safeHex = hex.take(2)
-    Integer pc = Integer.parseInt(safeHex, 16) << 21 >> 21
-	return pc / 2.55
-
-}
-
-
-private BigDecimal hexToBigDecimal(String hex) {
-
-    int d = Integer.parseInt(hex, 16) << 21 >> 21
-    return BigDecimal.valueOf(d)
-
-}
-
-
-private String hexToBinary(String thisByte, Integer size = 8) {
-
-	String binaryValue = new BigInteger(thisByte, 16).toString(2);
-	return String.format("%${size}s", binaryValue).replace(' ', '0')
-	
-}
-
-
-void loggingStatus() {
-
-	log.info  "${device} :  Info Logging : ${infoLogging == true}"
-	log.debug "${device} : Debug Logging : ${debugLogging == true}"
-	log.trace "${device} : Trace Logging : ${traceLogging == true}"
-
-}
-
-
-void traceLogOff(){
-	
-	log.trace "${device} : Trace Logging : Automatically Disabled"
-	device.updateSetting("traceLogging",[value:"false",type:"bool"])
-
-}
-
-void debugLogOff(){
-	
-	log.debug "${device} : Debug Logging : Automatically Disabled"
-	device.updateSetting("debugLogging",[value:"false",type:"bool"])
-
-}
-
-
-void infoLogOff(){
-	
-	log.info "${device} : Info  Logging : Automatically Disabled"
-	device.updateSetting("infoLogging",[value:"false",type:"bool"])
-
-}
-
-
-private boolean logging(String message, String level) {
-
-	boolean didLog = false
-
-	if (level == "error") {
-		log.error "$message"
-		didLog = true
-	}
-
-	if (level == "warn") {
-		log.warn "$message"
-		didLog = true
-	}
-
-	if (traceLogging && level == "trace") {
-		log.trace "$message"
-		didLog = true
-	}
-
-	if (debugLogging && level == "debug") {
-		log.debug "$message"
-		didLog = true
-	}
-
-	if (infoLogging && level == "info") {
-		log.info "$message"
-		didLog = true
-	}
-
-	return didLog
 
 }
