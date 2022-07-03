044620e6cc42e41e3081984045bdfc48d53c2143
Andrew Davison <andydvsn@users.noreply.github.com>
2022-07-03 03:08:18 +0100

    Update glow_mqtt.groovy
    Basic support for the cloud MQTT feed.

--- hildebrand/drivers/glow_mqtt.groovy
+++ hildebrand/drivers/glow_mqtt.groovy
@@ -1,6 +1,6 @@
 /*
  * 
- *  Hildebrand Glow MQTT Driver v1.02 (20th June 2022)
+ *  Hildebrand Glow MQTT Driver v1.03 (3rd July 2022)
  *	
  */
 
@@ -41,7 +41,8 @@ preferences {
 	input name: "mqttBroker", type: "text", title: "MQTT Broker Address:", required: true
 	input name: "mqttUser", type: "text", title: "MQTT Username:", required: false
 	input name: "mqttPass", type: "password", title: "MQTT Password:", required: false
-	//input name: "cloudId", type: "text", title: "Cloud ID:", required: false
+	input name: "cloudActive", type: "bool", title: "Use Hildebrand Cloud", defaultValue: false
+	input name: "cloudId", type: "text", title: "Cloud ID:", required: false
 
 	input name: "infoLogging", type: "bool", title: "Enable logging", defaultValue: true
 	input name: "debugLogging", type: "bool", title: "Enable debug logging", defaultValue: false
@@ -98,6 +99,12 @@ void initialize() {
 
 void configure() {
 
+	removeDataValue("firmware")
+	removeDataValue("mac")
+	removeDataValue("model")
+	removeDataValue("smetsversion")
+	removeDataValue("zigbee")
+
 	// Create child devices.
 	fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Electricity")
 	fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Gas")
@@ -124,10 +131,12 @@ void updated() {
 	}
 
 	// In case cloud support is added, which I am presently too tired to be thinking about.
-	if (settings?.cloudId) {
-		state.mqttTopic = "SMART/HILD/${cloudId}"
+	if (settings?.cloudActive) {
+		state.mqttBroker = "glowmqtt.energyhive.com"
+		state.mqttTopic = settings?.cloudId ? "SMART/HILD/${cloudId}" : ""
 		state.parsePasses = 1			// Cloud MQTT is received as 1 message and would need parsing in one pass.
 	} else {
+		state.mqttBroker = settings?.mqttBroker
 		state.mqttTopic = "glow/#"
 		state.parsePasses = 3			// Local MQTT is received as 3 seperate messages (STATE plus two SENSORS), parsed sequentially.
 	}
@@ -148,7 +157,7 @@ void disconnect() {
 	// Disconnect from broker.
 
 	interfaces.mqtt.disconnect()
-	logging("${device} : MQTT : Disconnected from broker $mqttBroker (${state.mqttTopic}).", "info")
+	logging("${device} : MQTT : Disconnected from broker ${state.mqttBroker} (${state.mqttTopic}).", "info")
 
 }
 
@@ -196,111 +205,158 @@ void parse(String description) {
 		
 		logging("${device} : JSON : ${json}", "debug")
 
-		if (msg.topic.indexOf('STATE') >= 0) {
+		def eleMeter = fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Electricity")
+		def gasMeter = fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Gas")
 
-			sendEvent(name: "lqi", value: "${json.han.lqi}")
-			sendEvent(name: "rssi", value: "${json.han.rssi}")
+		if (settings?.cloudActive) {
+
+			logging("${device} : Format : Cloud", "debug")
+
+			// State
+			int lqi = Integer.parseInt(json.pan.lqi, 16)
+			sendEvent(name: "lqi", value: lqi)
+
+			int rssi = Integer.parseInt(json.pan.rssi, 16)
+			String twosComplement = Integer.toHexString((-1 * rssi));
+			rssi = zigbee.convertHexToInt(twosComplement[-2..-1]) * -1
+			sendEvent(name: "rssi", value: rssi)
+
+			// Electricity
+			BigDecimal cumulative = Integer.parseInt(json.elecMtr."0702"."00"."00", 16) / 1000
+			cumulative = cumulative.setScale(3, BigDecimal.ROUND_HALF_UP)
+			eleMeter.parse([[name:"energy", value:cumulative]])
+
+			int power = Integer.parseInt(json.elecMtr."0702"."04"."00", 16)
+			eleMeter.parse([[name:"power", value:power]])
+
+			//logging("${device} : cumulative : $cumulative", "warn")
 
 			if (updateOccasional) {
 
-				updateDataValue("firmware", "${json.software}")
-				updateDataValue("mac", "${json.ethmac}")
-				updateDataValue("model", "${json.hardware}")
-				updateDataValue("smetsversion", "${json.smetsversion}")
-				updateDataValue("zigbee", "${json.zigbee}")
+				// State
+				updateDataValue("model", "${json.hversion}")
+				updateDataValue("smetsversion", "${json.smetsVer}")
+				updateDataValue("zigbee", "${json.zbSoftVer}")
+
+
+				// Electricity
+				String eleSupplier = json.elecMtr."0708"."01"."01"
+				eleMeter.setState([[name:"supplier", value:eleSupplier]])
+
+
+				// Gas
+				String gasSupplier = json.gasMtr."0708"."01"."01"
+				gasMeter.setState([[name:"supplier", value:gasSupplier]])
 
 			}
 
-		} else if (msg.topic.indexOf('electricitymeter') >= 0) {
+		} else {
 
-			def childDevice = fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Electricity")
+			logging("${device} : Format : Local", "debug")
 
-			BigDecimal cumulative = json.electricitymeter.energy.import.cumulative
-			childDevice.parse([[name:"energy", value:cumulative]])
+			if (msg.topic.indexOf('STATE') >= 0) {
 
-			BigDecimal power = json.electricitymeter.power.value * 1000
-			power = power.intValue()
-			childDevice.parse([[name:"power", value:power]])
+				sendEvent(name: "lqi", value: "${json.han.lqi}")
+				sendEvent(name: "rssi", value: "${json.han.rssi}")
 
-			if (updateOccasional) {
+				if (updateOccasional) {
 
-				BigDecimal day = json.electricitymeter.energy.import.day
-				BigDecimal week = json.electricitymeter.energy.import.week
-				BigDecimal month = json.electricitymeter.energy.import.month
+					updateDataValue("firmware", "${json.software}")
+					updateDataValue("mac", "${json.ethmac}")
+					updateDataValue("model", "${json.hardware}")
+					updateDataValue("smetsversion", "${json.smetsversion}")
+					updateDataValue("zigbee", "${json.zigbee}")
 
-				childDevice.parse([[name:"day", value:day]])
-				childDevice.parse([[name:"week", value:week]])
-				childDevice.parse([[name:"month", value:month]])
+				}
 
-				String mpan = json.electricitymeter.energy.import.mpan
-				childDevice.setState([[name:"mpan", value:mpan]])
+			} else if (msg.topic.indexOf('electricitymeter') >= 0) {
 
-				String units = json.electricitymeter.energy.import.units
-				childDevice.setState([[name:"energyUnit", value:"${units}"]])
-				childDevice.setState([[name:"powerUnit", value:"W"]]) 			// This is a fixed value but just in case it's easier to find here.
+				BigDecimal cumulative = json.electricitymeter.energy.import.cumulative
+				eleMeter.parse([[name:"energy", value:cumulative]])
 
-				BigDecimal unitrate = json.electricitymeter.energy.import.price.unitrate
-				BigDecimal unitratePence = unitrate * 100
-				unitratePence = unitratePence.setScale(2, BigDecimal.ROUND_HALF_UP)
-				childDevice.parse([[name:"unitrate", value:unitratePence]])
-				childDevice.setState([[name:"unitrateUnits", value:"p/kWh"]])
+				BigDecimal power = json.electricitymeter.power.value * 1000
+				power = power.intValue()
+				eleMeter.parse([[name:"power", value:power]])
 
-				BigDecimal standingcharge = json.electricitymeter.energy.import.price.standingcharge
-				BigDecimal standingchargePence = standingcharge * 100
-				standingchargePence = standingchargePence.setScale(2, BigDecimal.ROUND_HALF_UP)
-				childDevice.parse([[name:"standingcharge", value:standingchargePence]])
-				childDevice.setState([[name:"standingchargeUnits", value:"p/day"]])
+				if (updateOccasional) {
 
-				String supplier = json.electricitymeter.energy.import.supplier
-				childDevice.setState([[name:"supplier", value:supplier]])
+					BigDecimal day = json.electricitymeter.energy.import.day
+					BigDecimal week = json.electricitymeter.energy.import.week
+					BigDecimal month = json.electricitymeter.energy.import.month
 
-			}
+					eleMeter.parse([[name:"day", value:day]])
+					eleMeter.parse([[name:"week", value:week]])
+					eleMeter.parse([[name:"month", value:month]])
 
-		} else if (msg.topic.indexOf('gasmeter') >= 0) {
+					String mpan = json.electricitymeter.energy.import.mpan
+					eleMeter.setState([[name:"mpan", value:mpan]])
 
-			// Gas meters only send data every 30 minutes via the electricity meter, so there's no point being verbose.
-			// It is expected that cumulative energy will soon be sent in cubic meters and the power value will be removed.
+					String units = json.electricitymeter.energy.import.units
+					eleMeter.setState([[name:"energyUnit", value:"${units}"]])
+					eleMeter.setState([[name:"powerUnit", value:"W"]]) 			// This is a fixed value but just in case it's easier to find here.
 
-			if (updateOccasional) {
+					BigDecimal unitrate = json.electricitymeter.energy.import.price.unitrate
+					BigDecimal unitratePence = unitrate * 100
+					unitratePence = unitratePence.setScale(2, BigDecimal.ROUND_HALF_UP)
+					eleMeter.parse([[name:"unitrate", value:unitratePence]])
+					eleMeter.setState([[name:"unitrateUnits", value:"p/kWh"]])
 
-				def childDevice = fetchChild("BirdsLikeWires","Hildebrand Glow Meter","Gas")
+					BigDecimal standingcharge = json.electricitymeter.energy.import.price.standingcharge
+					BigDecimal standingchargePence = standingcharge * 100
+					standingchargePence = standingchargePence.setScale(2, BigDecimal.ROUND_HALF_UP)
+					eleMeter.parse([[name:"standingcharge", value:standingchargePence]])
+					eleMeter.setState([[name:"standingchargeUnits", value:"p/day"]])
 
-				BigDecimal cumulative = json.gasmeter.energy.import.cumulative
-				childDevice.parse([[name:"energy", value:cumulative]])
+					String supplier = json.electricitymeter.energy.import.supplier
+					eleMeter.setState([[name:"supplier", value:supplier]])
 
-				BigDecimal day = json.gasmeter.energy.import.day
-				BigDecimal week = json.gasmeter.energy.import.week
-				BigDecimal month = json.gasmeter.energy.import.month
+				}
 
-				childDevice.parse([[name:"day", value:day]])
-				childDevice.parse([[name:"week", value:week]])
-				childDevice.parse([[name:"month", value:month]])
+			} else if (msg.topic.indexOf('gasmeter') >= 0) {
 
-				String mprn = json.gasmeter.energy.import.mprn
-				childDevice.setState([[name:"mprn", value:mprn]])
+				// Gas meters only send data every 30 minutes via the electricity meter, so there's no point being verbose.
+				// It is expected that cumulative energy will soon be sent in cubic meters and the power value will be removed.
 
-				String units = json.gasmeter.energy.import.units
-				childDevice.setState([[name:"energyUnit", value:"${units}"]])
+				if (updateOccasional) {
 
-				BigDecimal unitrate = json.gasmeter.energy.import.price.unitrate
-				BigDecimal unitratePence = unitrate * 100
-				unitratePence = unitratePence.setScale(2, BigDecimal.ROUND_HALF_UP)
-				childDevice.parse([[name:"unitrate", value:unitratePence]])
-				childDevice.setState([[name:"unitrateUnits", value:"p/kWh"]])
+					BigDecimal cumulative = json.gasmeter.energy.import.cumulative
+					gasMeter.parse([[name:"energy", value:cumulative]])
 
-				BigDecimal standingcharge = json.gasmeter.energy.import.price.standingcharge
-				BigDecimal standingchargePence = standingcharge * 100
-				standingchargePence = standingchargePence.setScale(2, BigDecimal.ROUND_HALF_UP)
-				childDevice.parse([[name:"standingcharge", value:standingchargePence]])
-				childDevice.setState([[name:"standingchargeUnits", value:"p/day"]])
+					BigDecimal day = json.gasmeter.energy.import.day
+					BigDecimal week = json.gasmeter.energy.import.week
+					BigDecimal month = json.gasmeter.energy.import.month
 
-				String supplier = json.gasmeter.energy.import.supplier
-				childDevice.setState([[name:"supplier", value:supplier]])
+					gasMeter.parse([[name:"day", value:day]])
+					gasMeter.parse([[name:"week", value:week]])
+					gasMeter.parse([[name:"month", value:month]])
+
+					String mprn = json.gasmeter.energy.import.mprn
+					gasMeter.setState([[name:"mprn", value:mprn]])
+
+					String units = json.gasmeter.energy.import.units
+					gasMeter.setState([[name:"energyUnit", value:"${units}"]])
+
+					BigDecimal unitrate = json.gasmeter.energy.import.price.unitrate
+					BigDecimal unitratePence = unitrate * 100
+					unitratePence = unitratePence.setScale(2, BigDecimal.ROUND_HALF_UP)
+					gasMeter.parse([[name:"unitrate", value:unitratePence]])
+					gasMeter.setState([[name:"unitrateUnits", value:"p/kWh"]])
+
+					BigDecimal standingcharge = json.gasmeter.energy.import.price.standingcharge
+					BigDecimal standingchargePence = standingcharge * 100
+					standingchargePence = standingchargePence.setScale(2, BigDecimal.ROUND_HALF_UP)
+					gasMeter.parse([[name:"standingcharge", value:standingchargePence]])
+					gasMeter.setState([[name:"standingchargeUnits", value:"p/day"]])
+
+					String supplier = json.gasmeter.energy.import.supplier
+					gasMeter.setState([[name:"supplier", value:supplier]])
+
+				}
 
 			}
 
 		}
-					  
+	  
 	} catch (Exception e) {
 
 		logging("${device} : Parse : ${e.message}", "error")
@@ -332,12 +388,17 @@ void mqttConnect() {
 		def mqttInt = interfaces.mqtt
 
 		if (mqttInt.isConnected()) {
-			logging("${device} : MQTT : Connection to broker $mqttBroker (${state.mqttTopic}) is live.", "debug")
+			logging("${device} : MQTT : Connection to broker ${state.mqttBroker} (${state.mqttTopic}) is live.", "debug")
 			return
 		}
 
-		String clientID = "hubitat-" + device.deviceNetworkId		
-		mqttBrokerUrl = "tcp://" + settings?.mqttBroker + ":1883"
+		if (state.mqttTopic == "") {
+			logging("${device} : MQTT : Topic is not set.", "error")
+			return
+		}
+
+		String clientID = "hubitat-" + device.deviceNetworkId
+		mqttBrokerUrl = "tcp://" + state.mqttBroker + ":1883"
 		mqttInt.connect(mqttBrokerUrl, clientID, settings?.mqttUser, settings?.mqttPass)
 		pauseExecution(500)
 		mqttInt.subscribe(state.mqttTopic)
@@ -354,7 +415,7 @@ void mqttClientStatus(String status) {
 
 	if (status.indexOf('Connection succeeded') >= 0) {
 
-		logging("${device} : MQTT : Connection to broker $mqttBroker (${state.mqttTopic}) is live.", "debug")
+		logging("${device} : MQTT : Connection to broker ${state.mqttBroker} (${state.mqttTopic}) is live.", "debug")
 
 	} else {
 
