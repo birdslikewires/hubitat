e85e67244d977bf7eb85ed02c7e7cea8f3ed5f66
Andrew Davison <andydvsn@users.noreply.github.com>
2022-07-26 00:38:55 +0100

    Update alertme_smartplug.groovy

--- alertme/drivers/alertme_smartplug.groovy
+++ alertme/drivers/alertme_smartplug.groovy
@@ -9,7 +9,7 @@
 #include BirdsLikeWires.library
 import groovy.transform.Field
 
-@Field boolean debugMode = true
+@Field boolean debugMode = false
 @Field int reportIntervalMinutes = 2
 @Field int checkEveryMinutes = 1
 @Field int rangeEveryHours = 6
