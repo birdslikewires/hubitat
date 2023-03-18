62e21aa341375ae132821117501805533cee31f0
Andrew Davison <andydvsn@users.noreply.github.com>
2023-03-18 02:31:20 +0000

    Update tuya_remote.groovy

--- tuya/drivers/tuya_remote.groovy
+++ tuya/drivers/tuya_remote.groovy
@@ -5,12 +5,13 @@
  */
 
 
-@Field String driverVersion = "v1.02 (18th August 2021)"
+@Field String driverVersion = "v1.00 (18th March 2023)"
 
 
 #include BirdsLikeWires.library
 import groovy.transform.Field
 
+@Field String deviceName = "Tuya Remote"
 @Field boolean debugMode = true
 @Field int reportIntervalMinutes = 50
 @Field int checkEveryMinutes = 10
@@ -18,7 +19,7 @@ import groovy.transform.Field
 
 metadata {
 
-	definition (name: "Tuya Remote", namespace: "BirdsLikeWires", author: "Andrew Davison", importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/master/tuya/drivers/tuya_remote.groovy") {
+	definition (name: "$deviceName", namespace: "BirdsLikeWires", author: "Andrew Davison", importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/master/tuya/drivers/tuya_remote.groovy") {
 
 		capability "Configuration"
 		capability "HoldableButton"
@@ -27,6 +28,7 @@ metadata {
 		capability "PresenceSensor"
 		capability "PushableButton"
 		capability "ReleasableButton"
+		capability "SwitchLevel"
 
 	}
 
@@ -73,22 +75,20 @@ void processMQTT(def json) {
 	// Process the action first!
 	if (json.action) debounceAction("${json.action}")
 
-	sendEvent(name: "battery", value:"${json.battery}", unit: "%")
-
 	switch("${json.device.model}") {
 
-		case "E1766":
-			sendEvent(name: "numberOfButtons", value: 2, isStateChange: false)
+		case "FB20-002":
+			sendEvent(name: "numberOfButtons", value: 4, isStateChange: false)
 			break
 
-		case "E1812":
-			sendEvent(name: "numberOfButtons", value: 1, isStateChange: false)
+		default:
+			logging("${device} : Unknown device type '${json.device.model}'.", "warn")
 			break
 
 	}
 
-	String deviceName = "IKEA Tradfri Button ${json.device.model}"
-	if ("${device.name}" != "$deviceName") device.name = "$deviceName"
+	String thisDeviceName = "$deviceName ${json.device.model}"
+	if ("${device.name}" != "$thisDeviceName") device.name = "$thisDeviceName"
 	if ("${device.label}" != "${json.device.friendlyName}") device.label = "${json.device.friendlyName}"
 
 	updateDataValue("encoding", "MQTT")
@@ -99,5 +99,81 @@ void processMQTT(def json) {
 
 	updatePresence()
 	checkDriver()
+
+}
+
+
+@Field static Boolean debounceActionParsing = false
+void debounceAction(String action) {
+
+	if (debounceActionParsing) {
+		logging("${device} : parseMQTT : DEBOUNCED", "debug")
+		return
+	}
+	debounceActionParsing = true
+
+	switch(action) {
+
+		case "off":
+			logging("${device} : Action : Button 1 Pressed", "info")
+			sendEvent(name: "pushed", value: 1, isStateChange: true)
+			break
+
+		case "on":
+			logging("${device} : Action : Button 2 Pressed", "info")
+			sendEvent(name: "pushed", value: 2, isStateChange: true)
+			break
+
+		case "brightness_step_up":
+			logging("${device} : Action : Button 3 Pressed", "info")
+			sendEvent(name: "pushed", value: 3, isStateChange: true)
+			levelEvent(20,"increase")
+			break
+
+		case "brightness_move_up":
+			logging("${device} : Action : Button 3 Held", "info")
+			sendEvent(name: "held", value: 3, isStateChange: true)
+			state.levelChangeStart = now()
+			break
+
+		case "brightness_step_down":
+			logging("${device} : Action : Button 4 Pressed", "info")
+			sendEvent(name: "pushed", value: 4, isStateChange: true)
+			levelEvent(20,"decrease")
+			break
+
+		case "brightness_move_down":
+			logging("${device} : Action : Button 4 Held", "info")
+			sendEvent(name: "held", value: 4, isStateChange: true)
+			state.levelChangeStart = now()
+			break
+
+		case "brightness_stop":
+
+			int buttonNumber = device.currentState("held").value.toInteger()
+
+			logging("${device} : Action : Button ${buttonNumber} Released", "info")
+			sendEvent(name: "released", value: buttonNumber, isStateChange: true)
+
+			if (buttonNumber == 3) {
+
+				levelChange(100,"increase")
+
+			} else {
+
+				levelChange(100,"decrease")
+
+			}
+
+			break
+
+		default:
+			logging("${device} : Action : Type '$action' is an unknown action.", "warn")
+			break
+
+	}
+
+	pauseExecution 200
+	debounceActionParsing = false
 
 }
