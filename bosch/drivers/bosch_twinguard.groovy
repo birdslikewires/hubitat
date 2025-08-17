060947a52aa98fe984763afe77d338a1cae78daf
Andrew Davison <andydvsn@users.noreply.github.com>
2025-08-17 02:40:37 +0100

    Update bosch_twinguard.groovy

--- bosch/drivers/bosch_twinguard.groovy
+++ bosch/drivers/bosch_twinguard.groovy
@@ -23,15 +23,20 @@ metadata {
 		importUrl: "https://raw.githubusercontent.com/birdslikewires/hubitat/main/bosch/drivers/bosch_twinguard.groovy") {
 
 		capability "AirQuality"
+		capability "Alarm"
 		capability "Battery"
+		capability "CarbonDioxideMeasurement"
 		capability "Configuration"
 		capability "IlluminanceMeasurement"
 		capability "PowerSource"
 		capability "RelativeHumidityMeasurement"
 		capability "SignalStrength"
+		capability "SmokeDetector"
 		capability "TemperatureMeasurement"
 
 		attribute "healthStatus", "enum", ["offline", "online"]
+		attribute "siren", "string"
+		attribute "voc", "number"
 
 		if (debugMode) {
 			command "testCommand"
@@ -60,10 +65,10 @@ void testCommand() {
 
 void configureSpecifics() {
 
+	device.name = "$deviceName"
 	updateDataValue("encoding", "MQTT")
 	updateDataValue("isComponent", "false")
-
-	sendEvent(name: "powerSource", value:"battery")
+	sendEvent(name: "powerSource", value: "battery")
 
 }
 
@@ -79,20 +84,62 @@ void processMQTT(def json) {
 
 	checkDriver()
 
-	// Tasks
-	if (json.aqi) sendEvent(name: "airQualityIndex", value:"${json.aqi}")
-	if (json.humidity) sendEvent(name: "humidity", value:"${json.humidity}", unit: "%rh")
-	if (json.illuminance) sendEvent(name: "illuminance", value:"${json.illuminance}", unit: "lx")
-	if (json.temperature) sendEvent(name: "temperature", value:"${json.temperature}", unit: "°C")
+	// Smoke
+	if (json.smoke) {
 
-	// Administrative
-	if (json.battery) sendEvent(name: "battery", value:"${json.battery}", unit: "%")
+		sendEvent(name: "smoke", value: "detected")
+
+	} else {
+
+		String noSmoke = (json.self_test) ? "tested" : "clear"
+		sendEvent(name: "smoke", value: "$noSmoke")
+
+	}
+
+
+	if (json.siren_state) {
+
+		String alarmState
+		switch("${json.siren_state}") {
+
+			case "burglar":
+				alarmState = "strobe"
+				break
+
+			case "fire":
+				alarmState = "siren"
+				break						
+
+			default:
+				alarmState = "off"
+				break
+
+		}
+
+		sendEvent(name: "alarm", value: "$alarmState")
+		sendEvent(name: "siren", value: "${json.siren_state}")
+
+	}
+
+	// Environmental
+	if (json.aqi) sendEvent(name: "airQualityIndex", value: "${json.aqi}")
+	if (json.co2) sendEvent(name: "carbonDioxide", value: "${json.co2}", unit: "ppm")
+	if (json.humidity) sendEvent(name: "humidity", value: "${json.humidity}", unit: "%rh")
+	if (json.illuminance) sendEvent(name: "illuminance", value: "${json.illuminance}", unit: "lx")
+	if (json.temperature) sendEvent(name: "temperature", value: "${json.temperature}", unit: "°C")
+	if (json.voc) sendEvent(name: "voc", value: "${json.voc}", unit: "µg/m³")
+
+	// Device
+	if (json.battery) sendEvent(name: "battery", value: "${json.battery}", unit: "%")
 	if (json.linkquality) sendEvent(name: "lqi", value: "${json.linkquality}")
 
-	device.name = "${json.device.model}"
-	device.label = "${json.device.friendlyName}"
-	updateDataValue("manufacturer", "${json.device.manufacturerName}")
-	updateDataValue("model", "${json.device.model}")
+	if (json.device) {
+
+		if (json.device.friendlyName) device.label = "${json.device.friendlyName}"
+		if (json.device.manufacturerName) updateDataValue("manufacturer", "${json.device.manufacturerName}")
+		if (json.device.model) updateDataValue("model", "${json.device.model}")
+
+	}
 
 	logging("${device} : parseMQTT : ${json}", "debug")
 
