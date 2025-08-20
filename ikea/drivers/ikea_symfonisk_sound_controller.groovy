9482272e59258b45578899856b8867dbcb3d400f
Andrew Davison <andydvsn@users.noreply.github.com>
2025-08-20 01:42:55 +0100

    Stripped Zigbee processing

--- ikea/drivers/ikea_symfonisk_sound_controller.groovy
+++ ikea/drivers/ikea_symfonisk_sound_controller.groovy
@@ -5,7 +5,7 @@
  */
 
 
-@Field String driverVersion = "v1.11 (20th August 2025)"
+@Field String driverVersion = "v1.12 (20th August 2025)"
 @Field boolean debugMode = false
 
 #include BirdsLikeWires.library
@@ -62,20 +62,6 @@ void testCommand() {
 
 void configureSpecifics() {
 
-	String encodingCheck = "${getDeviceDataByName('encoding')}"
-
-	if (encodingCheck.indexOf('MQTT') >= 0) {
-
-		logging("${device} : configureSpecifics() : MQTT device, no Zigbee configuration required.", "trace")
-
-	} else {
-
-		int reportIntervalSeconds = reportIntervalMinutes * 60
-		sendZigbeeCommands(zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021, DataType.UINT8, reportIntervalSeconds, reportIntervalSeconds, 0x00))   // Report in regardless of other changes.
-		requestBasic()
-
-	}
-
 	sendEvent(name: "level", value: 0, isStateChange: false)
 
 }
@@ -90,216 +76,9 @@ void updateSpecifics() {
 
 void refresh() {
 
-	// Battery status can be requested if command is sent within about 3 seconds of an actuation.
-	sendZigbeeCommands(zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021))
 	logging("${device} : Refreshed", "info")
 
 }
-
-
-void parse(String description) {
-
-	updateHealthStatus()
-	checkDriver()
-	
-	logging("${device} : parse() : $description", "trace")
-
-	Map descriptionMap = zigbee.parseDescriptionAsMap(description)
-
-	if (descriptionMap) {
-
-		processMap(descriptionMap)
-
-	} else {
-		
-		reportToDev(descriptionMap)
-
-	}
-
-}
-
-
-void processMap(Map map) {
-
-	logging("${device} : processMap() : ${map}", "trace")
-
-	String[] receivedData = map.data
-
-	if (map.cluster == "0001") { 
-		// Power Configuration Cluster
-
-		if (map.attrId == "0021") {
-
-			reportBattery("${map.value}", 10, 2.1, 3.0)
-
-		} else {
-
-			logging("${device} : Skipped : Power Cluster with no data.", "debug")
-
-		}
-
-	} else if (map.clusterId == "0001") { 
-
-		logging("${device} : Skipped : Power Cluster with no data.", "debug")
-
-	} else if (map.clusterId == "0006") { 
-
-		if (map.command == "02") {
-
-			debouncePress(map)
-
-		} else {
-
-			logging("${device} : Skipped : On/Off Cluster with extraneous data.", "debug")
-
-		}
-
-	} else if (map.clusterId == "0008") {
-
-		debouncePress(map)
-
-	} else {
-
-		filterThis(map)
-
-	}
-
-}
-
-
-@Field static Boolean isParsing = false
-def debouncePress(Map map) {
-
-	if (isParsing) return
-	isParsing = true
-
-	// We'll figure out the button numbers in a tick.
-	int buttonNumber = 0
-
-	if (map.clusterId == "0006") { 
-
-		// This is a press of the button.
-		buttonNumber = 1
-
-		logging("${device} : Action : Button ${buttonNumber} Pressed", "info")
-		sendEvent(name: "pushed", value: buttonNumber, isStateChange: true)
-		
-		device.currentState("switch").value == "off" ? on() : off()
-
-	} else if (map.clusterId == "0008") {
-
-		if (map.command == "01") {
-
-			// This is a turn of the dial starting.
-			buttonNumber = 2
-
-			String[] receivedData = map.data
-
-			if (receivedData[0] == "00") {
-
-				buttonNumber = 2
-				state.levelChangeStart = now()
-				logging("${device} : Action : Dial (Button ${buttonNumber}) Turning Clockwise", "info")
-				sendEvent(name: "pushed", value: buttonNumber, isStateChange: true)
-				sendEvent(name: "direction", value: "clockwise")
-				sendEvent(name: "held", value: buttonNumber, isStateChange: true)
-				sendEvent(name: "switch", value: "on", isStateChange: false)
-
-			} else if (receivedData[0] == "01") {
-
-				buttonNumber = 3
-				state.levelChangeStart = now()
-				logging("${device} : Action : Dial (Button ${buttonNumber}) Turning Anticlockwise", "info")
-				sendEvent(name: "pushed", value: buttonNumber, isStateChange: true)
-				sendEvent(name: "direction", value: "anticlockwise")
-				sendEvent(name: "held", value: buttonNumber, isStateChange: true)
-				sendEvent(name: "switch", value: "on", isStateChange: false)
-
-			} else {
-
-				reportToDev(map)
-
-			}
-
-		} else if (map.command == "02") {
-
-			// This is a multi-press of the button.
-			buttonNumber = 1
-
-			String[] receivedData = map.data
-
-			if (receivedData[0] == "00") {
-
-				// Double-press is a supported Hubitat action, so just report the event.
-				logging("${device} : Action : Button ${buttonNumber} Double Pressed", "info")
-				sendEvent(name: "doubleTapped", value: buttonNumber, isStateChange: true)
-
-			} else if (receivedData[0] == "01") {
-
-				// Triple-pressing is not a supported Hubitat action, but this device doesn't support hold or release on the button, so we'll use "held" for this.
-				logging("${device} : Action : Button ${buttonNumber} Triple Pressed", "info")
-				sendEvent(name: "held", value: buttonNumber, isStateChange: true)
-
-			} else {
-
-				reportToDev(map)
-
-			}
-
-		} else if (map.command == "03") {
-
-			// This is a turn of the dial stopping.
-			// There's no differentiation in the data sent, so we work out from which direction we're stopping using the previous hold state.
-
-			buttonNumber = device.currentState("held").value.toInteger()
-
-			logging("${device} : Action : Dial (Button ${buttonNumber}) Stopped", "info")
-			sendEvent(name: "released", value: buttonNumber, isStateChange: true)
-
-			// Now work out the level we should change to based upon the time spent changing.
-
-			Integer initialLevel = device.currentState("level").value.toInteger()
-
-			long millisTurning = now() - state.levelChangeStart
-			if (millisTurning > 6000) {
-				millisTurning = 0				// In case the messages don't stop we could end up at full brightness or VOLUME!
-			}
-
-			BigInteger levelChange = 0
-			levelChange = millisTurning / 6000 * 100
-
-			BigDecimal targetLevel = 0
-
-			if (buttonNumber == 2) {
-
-				targetLevel = device.currentState("level").value.toInteger() + levelChange
-
-			} else {
-
-				targetLevel = device.currentState("level").value.toInteger() - levelChange
-				levelChange *= -1
-
-			}
-
-			logging("${device} : Level : Dial (Button ${buttonNumber}) - Changing from initialLevel '${initialLevel}' by levelChange '${levelChange}' after millisTurning for ${millisTurning} ms.", "debug")
-
-			sendEvent(name: "levelChange", value: levelChange, isStateChange: true)
-
-			setLevel(targetLevel)
-
-		} else {
-
-			reportToDev(map)
-
-		}
-
-	}
-
-	pauseExecution 110
-	isParsing = false
-
-}
-
 
 
 void processMQTT(def json) {
--- ikea/drivers/ikea_tradfri_button.groovy
+++ ikea/drivers/ikea_tradfri_button.groovy
@@ -5,7 +5,7 @@
  */
 
 
-@Field String driverVersion = "v1.19 (20th August 2025)"
+@Field String driverVersion = "v1.20 (20th August 2025)"
 @Field boolean debugMode = false
 
 #include BirdsLikeWires.library
@@ -36,10 +36,6 @@ metadata {
 			command "testCommand"
 		}
 
-		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,0020,1000,FC7C", outClusters: "0003,0004,0006,0008,0019,0102,1000", manufacturer: "IKEA of Sweden", model: "TRADFRI open/close remote", deviceJoinName: "IKEA Tradfri Open/Close Remote"
-		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,0020,1000", outClusters: "0003,0004,0006,0008,0019,0102,1000", manufacturer: "IKEA of Sweden", model: "TRADFRI SHORTCUT Button", deviceJoinName: "IKEA Tradfri Shortcut Button"
-		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,0020,1000,FC7C", outClusters: "0003,0004,0006,0008,0019,1000", manufacturer: "IKEA of Sweden", model: "TRADFRI SHORTCUT Button", deviceJoinName: "IKEA Tradfri Shortcut Button"
-
 	}
 
 }
@@ -63,16 +59,6 @@ void testCommand() {
 
 void configureSpecifics() {
 
-	String encodingCheck = "${getDeviceDataByName('encoding')}"
-
-	if (encodingCheck != "MQTT") {
-
-		int reportIntervalSeconds = reportIntervalMinutes * 60
-		sendZigbeeCommands(zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021, DataType.UINT8, reportIntervalSeconds, reportIntervalSeconds, 0x00))   // Report in regardless of other changes.
-		requestBasic()
-
-	}
-
 	sendEvent(name: "level", value: 0, isStateChange: false)
 
 }
@@ -87,180 +73,7 @@ void updateSpecifics() {
 
 void refresh() {
 
-	String encodingCheck = "${getDeviceDataByName('encoding')}"
-
-	if (encodingCheck != "MQTT") {
-
-		// Battery status can be requested if command is sent within about 3 seconds of an actuation.
-		sendZigbeeCommands(zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0020))
-
-	}
-
 	logging("${device} : Refreshed", "info")
-
-}
-
-
-void parse(String description) {
-
-	updateHealthStatus()
-	checkDriver()
-
-	logging("${device} : parse() : $description", "trace")
-
-	Map descriptionMap = zigbee.parseDescriptionAsMap(description)
-
-	if (descriptionMap) {
-
-		processMap(descriptionMap)
-
-	} else {
-		
-		reportToDev(descriptionMap)
-
-	}
-
-}
-
-
-void processMap(Map map) {
-
-	logging("${device} : processMap() : ${map}", "trace")
-
-	String[] receivedData = map.data
-
-	if (map.cluster == "0001") { 
-		// Power Configuration Cluster
-
-		if (map.attrId == "0020") {
-
-			reportBattery("${map.value}", 10, 2.1, 3.0)
-
-		} else {
-
-			logging("${device} : Skipped : Power Cluster with no data.", "debug")
-
-		}
-
-	} else if (map.clusterId == "0001") {
-
-		logging("${device} : Skipped : Power Cluster with no data.", "debug")
-
-	} else if (map.clusterId == "0006") {
-		// Tap and double-tap from the E1812
-
-		if (receivedData.length == 0) {
-
-			debouncePress(map)
-
-		} else {
-
-			logging("${device} : Skipped : On/Off Cluster with extraneous data.", "debug")
-
-		}
-
-	} else if (map.clusterId == "0008") {
-		// Hold and release from the E1812
-
-		if (map.command == "05" || map.command == "07") {
-
-			debouncePress(map)
-
-		} else {
-
-			filterThis(map)
-
-		}
-
-	} else if (map.clusterId == "0102") {
-		// Taps and releases from the E1766 (maybe others).
-		// I don't have one, so I can't test in person.
-
-		if (map.command == "00" || map.command == "01" || map.command == "02") {
-
-			debouncePress(map)
-
-		} else {
-
-			filterThis(map)
-
-		}
-
-	} else {
-
-		filterThis(map)
-
-	}
-
-}
-
-
-@Field static Boolean isParsing = false
-def debouncePress(Map map) {
-
-	if (isParsing) return
-	isParsing = true
-
-	if (map.clusterId == "0006") { 
-
-		if (map.command == "01") {
-
-			logging("${device} : Action : Button Pressed", "info")
-			sendEvent(name: "pushed", value: 1, isStateChange: true)
-
-		} else if (map.command == "00") {
-
-			logging("${device} : Action : Button Double-Pressed", "info")
-			sendEvent(name:"doubleTapped", value: 1, isStateChange:true)
-
-		}
-
-	} else if (map.clusterId == "0008") {
-
-		if (map.command == "05") {
-
-			logging("${device} : Action : Button Held", "info")
-			sendEvent(name: "held", value: 1, isStateChange: true)
-
-		} else if (map.command == "07") {
-
-			logging("${device} : Action : Button Released", "info")
-			sendEvent(name: "released", value: 1, isStateChange: true)
-
-		} else {
-
-			reportToDev(map)
-
-		}
-
-	} else if (map.clusterId == "0102") {
-
-		if (map.command == "00") {
-
-			sendEvent(name: "pushed", value: 1, isStateChange: true)
-			logging("${device} : Action : Button 1 Pressed", "info")
-
-		} else if (map.command == "01") {
-
-			sendEvent(name: "pushed", value: 2, isStateChange: true)
-			logging("${device} : Action : Button 2 Pressed", "info")
-
-		} else if (map.command == "02") {
-
-			int whichButton = device.currentState("pushed").value.toInteger()
-			sendEvent(name: "released", value: whichButton, isStateChange: true)
-			logging("${device} : Action : Button $whichButton Released", "info")
-
-		} else {
-
-			reportToDev(map)
-
-		}
-
-	}
-
-	pauseExecution 200
-	isParsing = false
 
 }
 
