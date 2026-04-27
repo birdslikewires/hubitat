1aac33d5c9c9dbe6b9c36f981b182cc003794dd4
Andrew Davison <andydvsn@users.noreply.github.com>
2026-04-27 09:56:03 +0100

    Version bump for library update

--- alertme/drivers/alertme_smartplug.groovy
+++ alertme/drivers/alertme_smartplug.groovy
@@ -5,7 +5,7 @@
  */
 
 
-@Field String driverVersion = "v1.70 (25th April 2026)"
+@Field String driverVersion = "v1.71 (25th April 2026)"
 @Field boolean debugMode = false
 
 #include BirdsLikeWires.alertme
