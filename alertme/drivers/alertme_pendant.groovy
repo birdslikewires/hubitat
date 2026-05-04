492a2f7a68f2bd4fe0afdfc26948de88bd2603a4
Andrew Davison <andydvsn@users.noreply.github.com>
2026-05-04 21:19:15 +0100

    Bump to trigger library update

--- alertme/drivers/alertme_pendant.groovy
+++ alertme/drivers/alertme_pendant.groovy
@@ -5,7 +5,7 @@
  */
 
 
-@Field String driverVersion = "v1.16 (28th April 2026)"
+@Field String driverVersion = "v1.17 (4th May 2026)"
 @Field boolean debugMode = false
 
 #include BirdsLikeWires.alertme
